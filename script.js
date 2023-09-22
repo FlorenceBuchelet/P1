8b8ced529b11c3decfc19cac9269b85b87933016
Florence Buchelet <florence.buchelet@gmail.com>
2023-09-22 13:51:29 +0200

    renaming elements, organization

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// tableaux données élèves (faut-il fusionner les tableaux en un seul ?)
+const student1 { name: "Thomas Nigon", quote: "If it bleeds, we can kill it.", linkedin: "https://www.linkedin.com/in/thomas-nigon/", github: "https://github.com/Thomas-Nigon" };
+const student2 { name: "Florence Buchelet", quote: "If you want to lift yourself up, lift up someone else.", linkedin: "https://www.linkedin.com/in/florence-buchelet-928a7a228/", github: "" };
+const student3 { name: "", quote: "", linkedin: "", github: "" };
+const student4 { name: "", quote: "", linkedin: "", github: "" };
+const student5 { name: "", quote: "", linkedin: "", github: "" };
+const student6 { name: "", quote: "", linkedin: "", github: "" };
+const student7 { name: "", quote: "", linkedin: "", github: "" };
+const student8 { name: "", quote: "", linkedin: "", github: "" };
