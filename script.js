d40dbc0fde717f3de4b09f94dbbbd88d509f3985
Florence Buchelet <florence.buchelet@gmail.com>
2023-09-25 17:06:45 +0200

    ajustement flex fond

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 // tableaux données élèves (faut-il fusionner les tableaux en un seul ?)
-const student1 {
+const student1 [{
     name: "Thomas Nigon",
     quote: "If it bleeds, we can kill it.",
     linkedin: "https://www.linkedin.com/in/thomas-nigon/",
@@ -10,34 +10,35 @@ const student2 {
     name: "Florence Buchelet",
     quote: "If you want to lift yourself up, lift up someone else.",
     linkedin: "https://www.linkedin.com/in/florence-buchelet-928a7a228/",
-    github: "" };
+    github: "https://github.com/FlorenceBuchelet" };
 const student3 {
     name: "Vincent Rousseaux",
     quote: "Mistakes are a fact of life. It is the response to the error that counts.",
     linkedin: "https://www.linkedin.com/in/vincent-rousseaux-500211172/",
     github: "https://github.com/VincentRssx" };
 const student4 {
-    name: "",
-    quote: "",
-    linkedin: "",
-    github: "" };
+    name: "Robin Fantino",
+    quote: "Viens, on va prendre une fritte.",
+    linkedin: "http://www.linkedin.com/in/robin-fantino-b351b625b",
+    github: "https://github.com/NemoOnTheNet" };
 const student5 {
-    name: "",
+    name: "Frédéric Ticon",
     quote: "",
     linkedin: "",
     github: "" };
 const student6 {
-    name: "",
+    name: "Farid Zian",
     quote: "",
     linkedin: "",
     github: "" };
 const student7 {
-    name: "",
+    name: "Marie Saillard",
     quote: "",
     linkedin: "",
     github: "" };
 const student8 {
-    name: "",
+    name: "Hélène ",
     quote: "",
     linkedin: "",
     github: "" };
+]
