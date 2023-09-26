6a1ebda66559ef67e77c49a56adda07c6a0e4415
Florence Buchelet <florence.buchelet@gmail.com>
2023-09-26 17:25:35 +0200

    update fond et corps polaroid

--- script.js
+++ script.js
@@ -1,44 +1,76 @@
 
 
-// tableaux données élèves (faut-il fusionner les tableaux en un seul ?)
-const student1 [{
-    name: "Thomas Nigon",
-    quote: "If it bleeds, we can kill it.",
-    linkedin: "https://www.linkedin.com/in/thomas-nigon/",
-    github: "https://github.com/Thomas-Nigon" };
-const student2 {
-    name: "Florence Buchelet",
-    quote: "If you want to lift yourself up, lift up someone else.",
-    linkedin: "https://www.linkedin.com/in/florence-buchelet-928a7a228/",
-    github: "https://github.com/FlorenceBuchelet" };
-const student3 {
+// tableaux données élèves
+const students [
+    {
+        name: "Thomas Nigon",
+        quote: "If it bleeds, we can kill it.",
+        linkedin: "https://www.linkedin.com/in/thomas-nigon/",
+        github: "https://github.com/Thomas-Nigon";
+picture: URL("assets/Thomas.jpeg")
+};
+{
+    name: "Hélène Rampelberg",
+        quote: "Salut c'est Hélène",
+            linkedin: "http://www.linkedin.com/",
+                github: "https://github.com/"
+    picture: URL("assets/Helene.jpeg")
+};
+{
     name: "Vincent Rousseaux",
-    quote: "Mistakes are a fact of life. It is the response to the error that counts.",
-    linkedin: "https://www.linkedin.com/in/vincent-rousseaux-500211172/",
-    github: "https://github.com/VincentRssx" };
-const student4 {
+        quote: "Mistakes are a fact of life. It is the response to the error that counts.",
+            linkedin: "https://www.linkedin.com/in/vincent-rousseaux-500211172/",
+                github: "https://github.com/VincentRssx";
+    picture: URL("assets/Vincent.jpeg")
+};
+{
     name: "Robin Fantino",
-    quote: "Viens, on va prendre une fritte.",
-    linkedin: "http://www.linkedin.com/in/robin-fantino-b351b625b",
-    github: "https://github.com/NemoOnTheNet" };
-const student5 {
+        quote: "Viens, on va prendre une frite.",
+            linkedin: "http://www.linkedin.com/in/robin-fantino-b351b625b",
+                github: "https://github.com/NemoOnTheNet";
+    picture: URL("assets/Robin.jpeg")
+};
+{
     name: "Frédéric Ticon",
-    quote: "",
-    linkedin: "",
-    github: "" };
-const student6 {
+        quote: "Salut c'est Fred",
+            linkedin: "http://www.linkedin.com/",
+                github: "https://github.com/";
+    picture: URL("assets/Fred.jpeg")
+};
+{
     name: "Farid Zian",
-    quote: "",
-    linkedin: "",
-    github: "" };
-const student7 {
+        quote: "Salut c'est Farid",
+            linkedin: "http://www.linkedin.com/",
+                github: "https://github.com/";
+    picture: URL("assets/Farid.jpeg")
+};
+{
     name: "Marie Saillard",
-    quote: "",
-    linkedin: "",
-    github: "" };
-const student8 {
-    name: "Hélène ",
-    quote: "",
-    linkedin: "",
-    github: "" };
+        quote: "Salut c'est Marie",
+            linkedin: "http://www.linkedin.com/",
+                github: "https://github.com/";
+    picture: URL("assets/Marie.jpeg")
+};
+{
+    name: "Florence Buchelet",
+        quote: "If you want to lift yourself up, lift up someone else.",
+            linkedin: "https://www.linkedin.com/in/florence-buchelet-928a7a228/",
+                github: "https://github.com/FlorenceBuchelet";
+    picture: URL("assets/Florence.jpeg")
+};
+
 ]
+
+
+/* 
+//tentative de génération auto des cards
+function updateCard(studentName, studentPicture) {
+    const name = document.querySelectorAll(h2);
+    const photo = document.querySelectorAll(".photo");
+    name.innerHTML = `${studentName}`;
+    photo.src = `url(${studentPicture})`;
+}
+
+for (let i = 0; i < students.length; i++) {
+    updateCard(students[i].name, students[i].picture);
+} */
